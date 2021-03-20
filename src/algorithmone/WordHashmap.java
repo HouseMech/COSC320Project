01d9f1fe583af375fe0235243eb92d9f35bd377b
Joels17 <joel.semeniuk@gmail.com>
2021-03-19 19:13:57 -0700

    Added file reader

--- src/algorithmone/WordHashmap.java
+++ src/algorithmone/WordHashmap.java
@@ -1,14 +1,30 @@
 package algorithmone;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class WordHashmap {
 	private Map<String,String> hashmap;
 
 	public WordHashmap() {
-		Map<String, String> hashmap = new HashMap<String, String>() {{
-		    put("btw", "by the way");
-		    put("ttyl", "talk to you later");
-		}};
+		Map<String, String> hashmap = new HashMap<String, String>();
+        try{
+            File f = new File("slangs.txt");
+            Scanner scan = new Scanner(f);
+            while(scan.hasNextLine()){
+                String data = scan.nextLine();
+                String a [] = data.split("\\s", 2);
+                if(a[0].equals("")){
+                    continue;
+                }else{
+                    hashmap.put(a[0], a[1]);
+                }
+                
+            }
+            scan.close();
+        }catch(FileNotFoundException e){
+            System.out.println(e);
+        }
 		this.hashmap = hashmap;
 	}
 
