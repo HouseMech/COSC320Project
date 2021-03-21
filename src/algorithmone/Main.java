2f2af081cbe414245133dc10bc46ce5ea6d75074
Liam Harbec <liam.harbec@gmail.com>
2021-03-21 16:43:36 -0700

    Added ability to read from CSV files

--- src/algorithmone/Main.java
+++ src/algorithmone/Main.java
@@ -1,32 +1,61 @@
 package algorithmone;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
+//TODO: Clean these things up
 public class Main {
 
 	public static void main(String[] args) {
+		String delimiter = ",";
 		Scanner sc = new Scanner(System.in); //use scanner purely for test purposes. Add list of tweets later.
 		String userInput = "";
 		String[] userInputArray;
 		String returnedValue;
+		
 		WordHashmap wordmap = new WordHashmap();
+		System.out.println("Input filename of csv");
 		while (true) {
 			userInput = sc.nextLine();
 			
-			userInputArray = userInput.split(" "); //Split string based on delimiter. This is the array we will feed into our algorithm.
-			for (int i = 0; i < userInputArray.length; i++) {
-				returnedValue = wordmap.getKey(userInputArray[i]);
-				if (returnedValue != null) { 
-					userInputArray[i] = returnedValue;
-				}
-			}
+			try {
+		         File file = new File(userInput);
+		         FileReader fr = new FileReader(file);
+		         BufferedReader br = new BufferedReader(fr);
+		         String line = "";
+		         String[] tempArr;
+		         br.readLine(); //Remove header line
+		         while((line = br.readLine()) != null) {
+		            tempArr = line.split(delimiter);
+		            userInputArray = tempArr[tempArr.length - 1].toLowerCase().split(" "); //Split string based on delimiter. This is the array we will feed into our algorithm.
+					for (int i = 0; i < userInputArray.length; i++) {
+						returnedValue = wordmap.getKey(userInputArray[i]);
+						if (returnedValue != null) { 
+							userInputArray[i] = returnedValue;
+						}
+						
+					}
+					for (int i = 0; i < userInputArray.length; i++) {
+						System.out.print(userInputArray[i] + " ");
+					}
+					System.out.println("\n");
+		            //System.out.println(tempArr[tempArr.length - 1]);
+		            //System.out.println();
+		         }
+		         br.close();
+		         
+		         
+		         } catch(IOException ioe) {
+		            ioe.printStackTrace();
+		         }
+		   }
+		
 			
-			for (int i = 0; i < userInputArray.length; i++) {
-				System.out.print(userInputArray[i] + " ");
-			}
-			System.out.println("\n");
 		}
-
-	}
+	
+	
 
  }
