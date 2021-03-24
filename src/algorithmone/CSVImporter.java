85394f6fed96258f7d32de28e5af9d28973c57ce
Liam Harbec <liam.harbec@gmail.com>
2021-03-24 14:07:56 -0700

    Added file writer

--- src/algorithmone/CSVImporter.java
+++ src/algorithmone/CSVImporter.java
@@ -3,35 +3,48 @@ package algorithmone;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class CSVImporter {
-	public static void readCSV(String userInput, String[] csvArray, WordHashmap wordmap) {
+	public static void readCSV(String fileName, String[] csvArray, WordHashmap wordmap) {
 		String delimiter = ",";
 		String returnedValue;
 		try {
-	        File file = new File(userInput);
+	        File file = new File("./inputcsv/" + fileName);
 	        FileReader fr = new FileReader(file);
 	        BufferedReader br = new BufferedReader(fr);
 	        String line = "";
-	        String[] tempArr;
+	        String[] columns;
+	        String editedTweet = "";
+	        FileWriter csvWriter = new FileWriter("./outputcsv/" + fileName);
+	        
+	        csvWriter.append("text");
+	        csvWriter.append("\n");
 	        br.readLine(); //Remove header line
 	        while((line = br.readLine()) != null) {
-	           tempArr = line.split(delimiter);
-	           csvArray = tempArr[tempArr.length - 1].toLowerCase().split(" "); //Split string based on delimiter. This is the array we will feed into our algorithm.
-				for (int i = 0; i < csvArray.length; i++) {
-					returnedValue = wordmap.getKey(csvArray[i]);
-					if (returnedValue != null) { 
-						csvArray[i] = returnedValue;
+	           columns = line.split(delimiter);
+	           if (columns.length == 6) {
+		           csvArray = columns[5].toLowerCase().split(" "); //Split string based on delimiter. This is the array we will feed into our algorithm.
+		           for (int i = 0; i < csvArray.length; i++) {
+						returnedValue = wordmap.getKey(csvArray[i]);
+						if (returnedValue != null) { 
+							csvArray[i] = returnedValue;
+						}
+						
 					}
-					
-				}
-				for (int i = 0; i < csvArray.length; i++) {
-					System.out.print(csvArray[i] + " ");
-				}
-				System.out.println("\n");
+		           for (String word : csvArray) {
+		        	   editedTweet += word + " "; //rebuild the new tweet with changed words
+		           }
+		           System.out.println(editedTweet);
+		           csvWriter.append(String.join(",", editedTweet));
+	               csvWriter.append("\n");
+	               editedTweet = "";
+	           }
 	        }
 	        br.close();
+	        csvWriter.flush();
+	        csvWriter.close();
 	        
 	        
 	        } catch(IOException ioe) {
--- src/algorithmone/Main.java
+++ src/algorithmone/Main.java
@@ -1,9 +1,6 @@
 package algorithmone;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
 
 //TODO: Clean these things up
@@ -11,18 +8,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in); //use scanner to get filenames
-		String userInput = "";
-		String[] csvArray = {};
 		
-		
+		File folder = new File("./inputcsv");
+		ArrayList<File> listOfFiles = new ArrayList<File>(); 
+		Collections.addAll(listOfFiles, folder.listFiles());
+		//File[] listOfFiles = 
 		WordHashmap wordmap = new WordHashmap();
-		System.out.println("Input filename of csv");
-		while (true) {
-			userInput = sc.nextLine();
-			CSVImporter.readCSV(userInput, csvArray, wordmap);
+		String[] csvArray = {};
+		long beforeTime;
+		long afterTime;
+		
+		Collections.sort(listOfFiles);
+		System.out.println(listOfFiles.toString());
+
+		for (int i = 0; i < 1; i++) {
+		  if (listOfFiles.get(i).isFile()) {
+			System.out.println("File " + listOfFiles.get(i).getName());
+			beforeTime = System.currentTimeMillis();
+		    CSVImporter.readCSV(listOfFiles.get(i).getName(), csvArray, wordmap);
+		    afterTime = System.currentTimeMillis();
+		    System.out.println("time taken = " + (afterTime - beforeTime));
+		  }
+		}
+		
+		
+		
+		
+		
+		
+		
 			
-		   }
+		   
 		
 			
 		}
